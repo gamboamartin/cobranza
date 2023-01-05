a9f12f0bc86310e3053636756b04dfea167b276d
[user] <angel.martinez@ivitec.com.mx>
2023-01-05 15:19:57 -0600

    v0.27.17 Se integra js para cliente

---
+++ js/cob_cliente/alta.js
@@ -0,0 +1,22 @@
+let text_razon_social = $("#razon_social");
+let text_cliente = $("#descripcion");
+let text_nombre = $("#nombre");
+let text_ap = $("#ap");
+let text_am = $("#am");
+
+
+text_nombre.change(function() {
+    text_razon_social.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+    text_cliente.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+});
+
+text_ap.change(function() {
+    text_razon_social.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+    text_cliente.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+});
+
+text_am.change(function() {
+    text_razon_social.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+    text_cliente.val(text_nombre.val() + ' ' + text_ap.val() + ' ' + text_am.val());
+});
+
