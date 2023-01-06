036a873d3c2be410ed8b21e5400aae61d622a3c3
javier <javier.flores@ivitec.com.mx>
2023-01-06 12:57:28 -0600

    v0.30.21 FIX en cob deuda, ahora el alta y modifica tienen el mismo orden

--- templates/inputs/cob_deuda/modifica.php
+++ templates/inputs/cob_deuda/modifica.php
@@ -1,11 +1,11 @@
 <?php /** @var  \gamboamartin\cobranza\controllers\controlador_cob_deuda $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 
-<?php echo $controlador->inputs->monto; ?>
-<?php echo $controlador->inputs->fecha_vencimiento; ?>
-
 <?php echo $controlador->inputs->cob_concepto_id; ?>
 <?php echo $controlador->inputs->cob_cliente_id; ?>
+
+<?php echo $controlador->inputs->monto; ?>
+<?php echo $controlador->inputs->fecha_vencimiento; ?>
 
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
 
